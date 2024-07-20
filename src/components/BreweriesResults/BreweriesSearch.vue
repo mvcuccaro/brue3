63864aed1b39f1ce8c7ef834225811f67f8c776e
mvcuccaro <9444185+mvcuccaro@users.noreply.github.com>
2024-07-20 17:10:52 -0400

    make search prettier / add search button

--- src/components/BreweriesResults/BreweriesSearch.vue
+++ src/components/BreweriesResults/BreweriesSearch.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="p-0 pt-4 pb-4" id="search">
-    <div class="col-sm-12 text-end pe-4">
-      Quick Search:
-      <input type="text" v-model="searchTerm" @keyup.enter="handleUpdate" />
+  <div class="p-2 pt-4 pb-4" id="search">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 text-end pe-4">
+        <div class="row">
+          <div class="col-xs-12  col-sm-8 p-2">
+            <input type="text" v-model="searchTerm" @keyup.enter="handleUpdate" class="form-control"/>
+          </div>
+          <div class="col-xs-12 p-2 col-sm-4 text-start">
+            <button class="btn btn-primary" style="width:100%" @click="handleUpdate">Search</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
